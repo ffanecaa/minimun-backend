c715e628925b76807d430521f728ffbad4afe338
marta <ffanecaa@gmail.com>
2023-03-23 14:11:39 +0100

    m

--- src/main.mjs
+++ src/main.mjs
@@ -8,11 +8,11 @@ app.use(express.json()) // Manexar os datos recibidos como JSON
 
 // O noso almacen de datos.
 // Normalmente en lugar do array atoparemos unha base de datos.
-let tarefas = [
+let tarefa = [
     {
         id: 0,
         descripcion: "Unha tarefa de exemplo",
-        completada: true,
+        completado: true,
     }
 ]
 
@@ -21,15 +21,15 @@ app.post("/tarefas/", controladorPost)
 app.get("/tarefas/", controladorGet)
 
 // Controladores executados polos endpoints
-function controladorPost (peticion, respuesta) {
-    tarefas.push(peticion.body)
-    respuesta.status(201)
-    respuesta.send("Ok")
+function controladorPost (peticion, resposta) {
+    tarefa.push(peticion.body)
+    resposta.status(201)
+    resposta.send("Ok")
 }
 
-function controladorGet (peticion, respuesta) {
-    respuesta.status(200)
-    respuesta.send(JSON.stringify(tarefas))
+function controladorGet (peticion, resposta) {
+    resposta.status(200)
+    resposta.send(JSON.stringify(tarefa))
 }
 
 
